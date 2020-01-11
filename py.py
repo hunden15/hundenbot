1f8870daec1c46f2b0fb647f47586699215a35cc
hunden15 <54918566+hunden15@users.noreply.github.com>
2020-01-11 18:30:31 +0900

    Update py.py

--- py.py
+++ py.py
@@ -15,8 +15,10 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.content.startswith("!정보"):
+  if message.content.startswith("!도움말"):
     await client.send_message(message.channel, "[도움말]\n!도움말 = 스넷봇 도움말을 확인합니다.")
+  if message.content.startswith("닥쳐"):
+    await client.send_message(message.channel, "욕하지마!!!!")
     
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
