8859e7a606fa18ad619e78db85cce20f5db536e6
hunden15 <54918566+hunden15@users.noreply.github.com>
2020-01-11 18:14:07 +0900

    Update py.py

--- py.py
+++ py.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import datatime
 
 
 client = discord.Client()
@@ -15,9 +16,15 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.content.startswith("s!"):
-    await client.send_message(message.channel, "제작자: 헌덴(HUNDEN)\n[도움말]\n1. s! -> 스넷봇의 정보를 확인합니다.\n2. s!info <유저닉네임> -> 유저의 정보를 확인합니다.")
-    
+  if message.content.startswith("s!info"):
+    data = datatime.datatime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
+    embed = discord.Embed(color=0x00ff00)
+    embed.addfield(name="이름", value=message.author.name, inline=True)
+    embed.addfield(name="서버닉네임", value=message.author.display_name, inline=True)
+    embed.addfield(name="가입일", value=str(data.year) + "년" + str(data.month) + "월" + str(data.day) + "일", inline=True)
+    embed.addfield(name="아이디", value=message.author.id, inline=True)
+    embed.set_thumbnail(url=message.author.avatar_url)
+    await client.send_message(message.channel, embed=embed)
     
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
