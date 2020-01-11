e83437d60b2aba8636df0636da766884ec00dc66
hunden15 <54918566+hunden15@users.noreply.github.com>
2020-01-11 18:28:14 +0900

    Update py.py

--- py.py
+++ py.py
@@ -1,6 +1,5 @@
 import discord
 import os
-import datatime
 
 
 client = discord.Client()
@@ -17,14 +16,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
   if message.content.startswith("!정보"):
-    data = datatime.datatime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
-    embed = discord.Embed(color=0x00ff00)
-    embed.addfield(name="이름", value=message.author.name, inline=True)
-    embed.addfield(name="서버닉네임", value=message.author.display_name, inline=True)
-    embed.addfield(name="가입일", value=str(data.year) + "년" + str(data.month) + "월" + str(data.day) + "일", inline=True)
-    embed.addfield(name="아이디", value=message.author.id, inline=True)
-    embed.set_thumbnail(url=message.author.avatar_url)
-    await client.send_message(message.channel, embed=embed)
+    await client.send_message(message.channel, "[도움말]\n!도움말 = 스넷봇 도움말을 확인합니다.")
     
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
