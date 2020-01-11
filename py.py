c177cce6c4288ac98a819ac2514b6aa01d2d5fe9
hunden15 <54918566+hunden15@users.noreply.github.com>
2020-01-11 19:57:05 +0900

    Update py.py

--- py.py
+++ py.py
@@ -16,7 +16,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
   if message.content.startswith("!도움말"):
-    await client.send_message(message.channel, "[도움말]\n!도움말 = 스넷봇 도움말을 확인합니다.")
+    await client.send_message(message.channel, "[도움말]\n!도움말 = 스넷봇 도움말을 확인합니다.\n\n[ 문의는 디스코드봇 1대1채팅으로 해주세요. ]")
   if message.content.startswith("닥쳐"):
     await client.send_message(message.channel, "욕하지마!!!!")
   if message.channel.is_private and message.author.id != "665460521050439710":
@@ -25,6 +25,8 @@ async def on_message(message):
     if message.author.id == "419810897058463754":
       member = discord.utils.get(client.get_all_members(), id=message.content[4:22])
       await client.send_message(member, "[스넷봇] 제작자 답변 : " + message.content[23:])
+    else:
+      await client.send_message(message.channel, "[스넷봇] 당신은 이 명령어를 사용할 권한이 없습니다."
     
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
