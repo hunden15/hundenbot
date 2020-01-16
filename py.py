669a99bbd70f4e7c5f5523662179e5d550d03f39
hunden15 <54918566+hunden15@users.noreply.github.com>
2020-01-16 21:07:39 +0900

    Update py.py

--- py.py
+++ py.py
@@ -1,18 +1,42 @@
 import discord
-from discord.ext import commands
+import os
+import datetime
+import asyncio
 
-client = commands.Bot(command_prefix = '&')
+
+client = discord.Client()
+
 
 @client.event
 async def on_ready():
-  print('Online')
+  print("login")
+  print(client.user.name)
+  print(client.user.id)
+  print("------------------")
+  await client.change_presence(game=discord.Game(name='', type=1))
 
-@client.command()
-@commands.has_permission(manage_messages=True)
-async def clear(ctx, amount=3):
-  await ctx.channel.purge(limit=amount)
-
-
-
+@client.event
+async def on_message(message):
+  SN = "`[스넷봇 시스템]"
+  if message.content == "=":
+    await client.send_message(message.channel, SN + " 기본 명령어: =help")
+  else:
+    if message.content[1:4] == "help":
+      embed = discord.Embed(color=0x0028ff)
+      embed.add_field(name="제작자", value="[스넷봇 도움말 시스템]\n=help :: 도움말 확인을 합니다.\n=info :: 나의 정보를 확인합니다.\n=contact :: 제작자에게 문의를 합니다.[봇 갠디코로만 가능]", inline=True)
+      await client.send_message(message.channel, embed=embed)
+    if message.content[1:3] == "dm":
+      if message.author.id == "419810897058463754":
+        member = discord.utils.get(client.get_all_members(), id=message.content[3:19])
+        await client.send_message(member, SN + " 제작자 : " + message.content[20:])
+      else:
+        await client.send_message(message.channel, SN + " [ " + message.author.name + " ] 님 당신은 이 명령어를 사용할 권한이 없습니다.")
+    if message.content[1:7] == "contact":
+      if message.content[8:]:
+        if message.channel.is_private and message.author.id != "665768509707518033":
+          await client.send_message(discord.utils.get(client.get_all_members(), id="419810897058463754"), message.author.name + "(" + message.author.id + ") : " + message.content[8:])
+      else:
+        await client.send_message(message.channel, SN + " 제작자에게 문의 보낼 메세지를 적어주세요.")
+        
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
