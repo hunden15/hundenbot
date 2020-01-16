671c74ec67f2375644455ab7cbb8ffa0c7298411
hunden15 <54918566+hunden15@users.noreply.github.com>
2020-01-16 21:48:41 +0900

    Update py.py

--- py.py
+++ py.py
@@ -16,45 +16,8 @@ async def on_ready():
   await client.change_presence(game=discord.Game(name='', type=1))
 
 @client.event
-async def on_member_join(member):
-  with open('users.json', 'r') as f:
-    users = json.load(f)
-  
-  await update_data(users, member)
-  
-  with open('users.json', 'w') as f:
-    json.dump(users, f)
-
-async def update_data(users, user):
-  if not user.id in users:
-    users[user.id] = {}
-    users[user.id]['experience'] = 0
-    users[user.id]['level'] = 1
-
-async def add_experience(users, user, exp):
-  users[user.id]['experience'] += exp
-
-async def level_up(users, user, channel):
-  experience = users[user.id]['experience']
-  lvl_start = users[user.id]['level']
-  lvl_end = int(experience ** (1/4))
-  
-  if lvl_start < lvl_end:
-    await client.send_message(channel, '{} has leveled up to level {}'.format(user.mention, lvl_end))
-    users[user.id]['level'] = lvl_end
-
-@client.event
 async def on_message(message):
   SN = "`[스넷봇 시스템]"
-  with open('users.json', 'r') as f:
-    users = json.load(f)
-  
-  await update_data(users, message.author)
-  await add_experience(users, message.author, 5)
-  await level_up(users, message.author, message.channel)
-  
-  with open('users.json', 'w') as f:
-    json.dump(users, f)
     
   if message.content == "=":
     await client.send_message(message.channel, SN + " 기본 명령어: =help`")
