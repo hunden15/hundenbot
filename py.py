665e68b29d3d5bfcef473f3465aea760a0b34ce0
hunden15 <54918566+hunden15@users.noreply.github.com>
2020-01-16 21:43:44 +0900

    Update py.py

--- py.py
+++ py.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import datetime
+import json
 
 
 client = discord.Client()
@@ -15,8 +16,46 @@ async def on_ready():
   await client.change_presence(game=discord.Game(name='', type=1))
 
 @client.event
+async def on_member_join(member):
+  with open('users.json', 'r') as f:
+    users = json.load(f)
+  
+  await update_data(users, member)
+  
+  with open('users.json', 'w') as f:
+    json.dump(users, f)
+
+async def update_data(users, user):
+  if not user.id in users:
+    users[user.id] = {}
+    users[user.id]['experience'] = 0
+    users[user.id]['level'] = 1
+
+async def add_experience(users, user, exp):
+  users[user.id]['experience'] += exp
+
+async def level_up(users, user, channel):
+  experience = users[user.id]['experience']
+  lvl_start = users[user.id]['level']
+  lvl_end = int(experience ** (1/4))
+  
+  if lvl_start < lvl_end:
+    await client.send_message(channel, '{} has leveled up to level {}'.format(user.mention, lvl_end))
+    users[user.id]['level'] = lvl_end
+
+@client.event
 async def on_message(message):
   SN = "`[스넷봇 시스템]"
+  with open('users.json', 'r') as f:
+    users = json.load(f)
+  
+  await update_data(users, message.author)
+  await add_experience(users, message.author, 5)
+  await level_up(users, message.author, message.channel)
+  
+  with open('users.json', 'w') as f:
+    json.dump(users, f)
+    
   if message.content == "=":
     await client.send_message(message.channel, SN + " 기본 명령어: =help`")
   else:
@@ -36,11 +75,8 @@ async def on_message(message):
           await client.send_message(discord.utils.get(client.get_all_members(), id="419810897058463754"), message.author.name + "(" + message.author.id + ") : " + message.content[8:])
       else:
         await client.send_message(message.channel, SN + " 제작자에게 문의 보낼 메세지를 적어주세요.`")
-    if message.content[1:6] == "clear":
-      if message.author.id == "419810897058463754":
-        await send_message(message.channel, limit=message.content[6:])
-      else:
-        await send_message(message.channel, SN + " " + message.author.name + "님 당신은 이 명령어를 사용할 권한이 없습니다.`")
+    if message.content[1:6] == "":
+      
         
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
