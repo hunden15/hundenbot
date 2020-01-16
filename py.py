24408eae95428e792cb20791533156e2e83b8479
hunden15 <54918566+hunden15@users.noreply.github.com>
2020-01-16 20:50:13 +0900

    Update py.py

--- py.py
+++ py.py
@@ -12,29 +12,6 @@ async def on_ready():
 async def clear(ctx, amount=3):
   await ctx.channel.purge(limit=amount)
 
-@client.command()
-async def kick(ctx, member : discord.Member, *, reason=None):
-  await member.kick(reason=reason)
-  await ctx.send(f'Kicked {member.mention}')
-
-@client.command()
-async def ban(ctx, member : discord.Member, *, reason=None):
-  await member.ban(reason=reason)
-  await ctx.send(f' Banned (member.mention}')
-
-@client.command()
-async def unban(ctx, *, member):
-  banned_users = await ctx.guild.bans()
-  member_name, member_discriminator = member.split('#')
-  
-  for ban_entry in banned_users:
-    user = ban_entry.user
-    
-    if (user.name, user.discriminator) == (member_name, member_discriminator):
-      await ctx.guild.unban(user)
-      await ctx.send(f' Unbanned {user.mention}')
-      return
-
 
 
 access_token = os.environ["BOT_TOKEN"]
